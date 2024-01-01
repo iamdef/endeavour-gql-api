0ee015aeca15c9d7dbe5fb748de0be5d8826214d
Dmitry <xolodetzzz@yandex.ru>
2024-01-01 22:15:32 +0300

    Remove Token.php from repository

--- src/App/utils/Token.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\utils;
-
-class Token {
-    public static function base64url_encode($data) { // функция кодировки
-        return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
-    }
-}
