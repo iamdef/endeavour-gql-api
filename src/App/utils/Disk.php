0c73405386d650ea0f161835939f99094e7a64b7
Dmitry <xolodetzzz@yandex.ru>
2025-01-18 12:36:27 +0300

    Removed needless require_once 'Vendor/autoload.php

--- src/App/utils/Disk.php
+++ src/App/utils/Disk.php
@@ -6,7 +6,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'Vendor/autoload.php';
+// require_once 'Vendor/autoload.php';
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable('Vendor/' . '../');
